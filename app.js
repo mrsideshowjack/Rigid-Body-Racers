b98f33dc5bc80afea8817e95d94c94410cfa70fd
Jack Mason <mrsideshowjack@gmail.com>
2018-02-09 18:15:23 +0000

    video chat

--- app.js
+++ app.js
@@ -26,14 +26,12 @@ function Player(id) {
     this.z = 0;
     this.entity = null;
 }
-
+/*************************/
+var channels = {};
+var sockets = {};
 io.sockets.on('connection', function (socket) {
 
-    socket.on('testreq', function (data) {
-        console.log('test page req:' + data);
-        socket.broadcast.emit('testres', 'socket success');
-    });
-
+    // GAME LOGIC
     socket.on('initialize', function () {
         var idNum = players.length;
         var newPlayer = new Player(idNum);
@@ -51,9 +49,88 @@ io.sockets.on('connection', function (socket) {
         socket.broadcast.emit('playerMoved', data);
     });
 
-    socket.on('disconnect', function () {
-        socket.broadcast.emit('killPlayer', data);
+    socket.on('disconnect', function (data) {
+        if (data) {
+            socket.broadcast.emit('killPlayer', data);
+        }
      })
+
+
+    // VIDEO CHAT LOGIC 
+    socket.channels = {};
+    sockets[socket.id] = socket;
+
+    console.log("[" + socket.id + "] connection accepted");
+    socket.on('disconnect', function () {
+        for (var channel in socket.channels) {
+            part(channel);
+        }
+        console.log("[" + socket.id + "] disconnected");
+        delete sockets[socket.id];
+    });
+
+
+    socket.on('join', function (config) {
+        console.log("[" + socket.id + "] join ", config);
+        var channel = config.channel;
+        var userdata = config.userdata;
+
+        if (channel in socket.channels) {
+            console.log("[" + socket.id + "] ERROR: already joined ", channel);
+            return;
+        }
+
+        if (!(channel in channels)) {
+            channels[channel] = {};
+        }
+
+        for (id in channels[channel]) {
+            channels[channel][id].emit('addPeer', { 'peer_id': socket.id, 'should_create_offer': false });
+            socket.emit('addPeer', { 'peer_id': id, 'should_create_offer': true });
+        }
+
+        channels[channel][socket.id] = socket;
+        socket.channels[channel] = channel;
+    });
+
+    function part(channel) {
+        console.log("[" + socket.id + "] part ");
+
+        if (!(channel in socket.channels)) {
+            console.log("[" + socket.id + "] ERROR: not in ", channel);
+            return;
+        }
+
+        delete socket.channels[channel];
+        delete channels[channel][socket.id];
+
+        for (id in channels[channel]) {
+            channels[channel][id].emit('removePeer', { 'peer_id': socket.id });
+            socket.emit('removePeer', { 'peer_id': id });
+        }
+    }
+    socket.on('part', part);
+
+    socket.on('relayICECandidate', function (config) {
+        var peer_id = config.peer_id;
+        var ice_candidate = config.ice_candidate;
+        console.log("[" + socket.id + "] relaying ICE candidate to [" + peer_id + "] ", ice_candidate);
+
+        if (peer_id in sockets) {
+            sockets[peer_id].emit('iceCandidate', { 'peer_id': socket.id, 'ice_candidate': ice_candidate });
+        }
+    });
+
+    socket.on('relaySessionDescription', function (config) {
+        var peer_id = config.peer_id;
+        var session_description = config.session_description;
+        console.log("[" + socket.id + "] relaying session description to [" + peer_id + "] ", session_description);
+
+        if (peer_id in sockets) {
+            sockets[peer_id].emit('sessionDescription', { 'peer_id': socket.id, 'session_description': session_description });
+        }
+    });
+     
 });
 
 
